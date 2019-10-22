892e9afe5db6baa3d09074b0ec3c77a71782ab62
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-10-22 17:41:40 -0500

    creating buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+const questions = [
+   " What is Joey's catchphrase?",
+   "What instrument does Phoebe play?",
+   "What is the name of the coffee house the Friends hangout at?",
+   "Where do the Friends all live?",
+   "Which pets do Joey and Chandler have?",
+   "Which of Phoebe's songs gets made into a music video?",
+   "Which Friend used to be fat?",
+   "Who does Phoebe end up with?",
+   "Where do Ross and Rachel get married?",
+   "Who have been friends since college?",
+];
