9b975695325ba27b3e41799c3b320b3d130c9942
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-10-23 01:14:46 -0500

    creating variables in javascript file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,61 @@
+const Number1 = {
+  question: " What is Joey's catchphrase?", 
+  correctAnswer: " How you doin?",
+  fakeAnswer: ["How are ya?", "Hey what's up?", "How’s it hanging?"],
+}
+const Number2 = {
+    question: "What instrument does Phoebe play?", 
+    correctAnswer: " Guitar",
+    fakeAnswer: ["Violin", "Piano", "Ukulele"],
+  }
+  const Number3 = {
+    question: "What is the name of the coffee house the Friends hangout at?", 
+    correctAnswer: " Central Perk ",
+    fakeAnswer:["Starbucks", "Central Coffee", "NYC Perks"],
+  }
+  const Number4 = {
+    question: "Where do the Friends all live?", 
+    correctAnswer: "New York City",
+    fakeAnswer:["Chicago", "Los Angeles", "Houston"],
+  }
+  const Number5 = {
+    question: "Which pets do Joey and Chandler have?", 
+    correctAnswer: "A chick and a duck",
+    fakeAnswer:["Dogs", "Cats", "Rat babies"],
+  }
+  const Number6 = {
+    question: "Which of Phoebe's songs gets made into a music video?", 
+    correctAnswer: "Smelly Cat ",
+    fakeAnswer:["Your Love", "ShowerSong", "Little Black curly hair"],
+  }
+  const Number7 = {
+    question: "Which Friend used to be fat?", 
+    correctAnswer: " Monica",
+    fakeAnswer:["Rachel", "Joey", "Chandler"],
+  }
+  const Number8 = {
+    question: "Who does Phoebe end up with?", 
+    correctAnswer: "Mike",
+    fakeAnswer:["Joey", "David", "Duncan"],
+  }
+  const Number9 = {
+    question: "Where do Ross and Rachel get married?", 
+    correctAnswer: "Vegas",
+    fakeAnswer: ["London", "Church Outside", "Central Perk?"],
+  }
+  const Number10 = {
+    question: "Who have been friends since college?", 
+    correctAnswer: "Ross and Chandler",
+    fakeAnswer:["Joey and Chandler", "Ross and Rachel", "Monica and Joey"],
+  }
 const questions = [
-   " What is Joey's catchphrase?",
-   "What instrument does Phoebe play?",
-   "What is the name of the coffee house the Friends hangout at?",
-   "Where do the Friends all live?",
-   "Which pets do Joey and Chandler have?",
-   "Which of Phoebe's songs gets made into a music video?",
-   "Which Friend used to be fat?",
-   "Who does Phoebe end up with?",
-   "Where do Ross and Rachel get married?",
-   "Who have been friends since college?",
-];
+Number1,
+Number2,
+Number3,
+Number4,
+Number5,
+Number6,
+Number7,
+Number8,
+Number9,
+Number10]
